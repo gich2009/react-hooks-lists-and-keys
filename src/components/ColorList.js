6af74f51bd0045ba9c8edbf69ff846436c3ca1c2
gich2009 <waneneamos@gmail.com>
2023-04-23 13:15:25 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+//Instead of using props, I destructured the props object in the parameter list of the ColorItem component.
+function ColorItem({color}){
+  return <li style={{color: color}}>{color}</li>;
+}
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,15 +14,19 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = colors.map((color) => <ColorItem key={color} color={color}/>);
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
+        {colorElements}
+        {/*The lines below were the original code but they have been commented out for the code along.*/}
+        {/* <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li> */}
       </ol>
     </div>
   );
